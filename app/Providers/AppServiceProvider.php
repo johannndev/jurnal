f022f3c96b4381d5bafbaf1885bc0697600d5b6a
johan sinaga <johanfransiskussinaga@gmail.com>
2025-08-31 14:55:09 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Assets\Js;
+use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
      public function boot(): void
     {
+
+        Livewire::setScriptRoute(function ($handle) {
+            return Route::get('/vendor/livewire/livewire.js', $handle);
+        });
 
          FilamentAsset::register([
             Js::make('custom-script', __DIR__ . '/../../resources/js/custom.js'),
