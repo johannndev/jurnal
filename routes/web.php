4edf866bff77e3752b61bd06796fa4e86d8d58db
johan sinaga <johanfransiskussinaga@gmail.com>
2025-08-31 15:00:40 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 Route::get('/', function () {
     return redirect()->route('filament.admin.auth.login');
+});
+
+Livewire::setScriptRoute(function ($handle) {
+    return Route::get('/public/vendor/livewire/livewire.js', $handle);
+});
+
+Livewire::setUpdateRoute(function ($handle) {
+    return Route::post('/public/vendor/livewire/update', $handle);
 });
 
 Route::get('/get-bank-saldo/{id}', function ($id) {
